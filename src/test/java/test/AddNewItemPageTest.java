390d0d4b3577d4c22866d081778ad67b1df50758
StevenIram1 <steven.Iram13@yahoo.com>
2022-01-04 09:26:00 -0600

    my second commit

--- src/test/java/test/AddNewItemPageTest.java
+++ src/test/java/test/AddNewItemPageTest.java
@@ -2,16 +2,16 @@ package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
+//import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
+//import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import page.AddNewItemPage;
 import util.BrowserFactory;
 
-  @Listeners(util.MyTestNGListeners.class) //Using Listeners on the class level
+  //@Listeners(util.MyTestNGListeners.class) //Using Listeners on the class level
 public class AddNewItemPageTest {
 
 	WebDriver driver;
